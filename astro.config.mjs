9d75bdb857881794fb75c4c13b0f951a7699e930
ItsLexus <105422453+ItsLexus@users.noreply.github.com>
2022-09-15 20:30:53 +0200

    added partytown and google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import preact from "@astrojs/preact";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact()]
+  integrations: [
+    tailwind(),
+    preact(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
